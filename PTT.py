dec44389b532ba94d74fde850ede9c3a1f05f24c
shihs <mcshihs@gmail.com>
2019-09-19 15:02:00 +0200

    some edit
    1. change function get_posts() name to get_posts_info()
    2. edit some function explanation
    3. separate push_ipdatetime to push_ip, push_date and push_time

--- PTT.py
+++ PTT.py
@@ -5,11 +5,10 @@ from bs4 import BeautifulSoup
 
 
 def get_board(board, pages):
-	'''爬取ptt各版文章推文數、標題、作者
+	'''爬取ptt各版文章資訊
 	Args:
 		borad:看板英文名稱
 		pages:要爬取的頁數，包含最新頁總共的頁數
-
 	'''
 	s = requests.Session()
 	# 看板
@@ -33,7 +32,7 @@ def get_board(board, pages):
 	get_page(s, res, board, pages)
 
 
-def get_posts(s, page_url):
+def get_posts_info(s, page_url):
 	'''獲取該頁文章標題、推文數、作者id
 	Args:
 		s:get_pages function的Session
@@ -42,7 +41,8 @@ def get_posts(s, page_url):
 	res = s.get(page_url)
 	soup = BeautifulSoup(res.text, "lxml")
 
-	# 獲取文章標題、推文數、作者
+
+	# 獲取文章標題、推文數、作者、文章網址
 	for post in soup.select(".r-ent")[:1]:
 		# python 2
 		# push = post.select(".nrec")[0].text.encode("utf-8").strip()
@@ -50,8 +50,9 @@ def get_posts(s, page_url):
 		# author = post.select(".author")[0].text.encode("utf-8").strip()
 		
 		# python 3
-		push = post.select(".nrec")[0].text.strip() 
+		push = post.select(".nrec")[0].text.strip()
 		title = post.select(".title")[0]
+		# 文章的網址
 		post_url = title.select("a")[0]["href"]
 		post_url = "https://www.ptt.cc" + post_url
 		# print (post_url)
@@ -79,15 +80,16 @@ def get_page(s, res, board, pages):
 	for page in range(pages):
 		page_url = "https://www.ptt.cc/bbs/" + board + "/index" + str(previous - page + 1) + ".html"
 		print (page_url)
-		get_posts(s, page_url)
+		get_posts_info(s, page_url)
 
 
 
 
 def get_post_content(s, post_url):
-	'''獲取每篇文章內容，包含po文時間、作者ip、文章內容、推文內容
-
-
+	'''獲取每篇文章詳細資訊，包含po文時間、作者ip、文章內容、推文內容
+	Args:
+		s:get_pages function的Session
+		post_url:the url of a post
 	'''
 	res = s.get(post_url)
 	soup = BeautifulSoup(res.text, "lxml")
@@ -100,37 +102,34 @@ def get_post_content(s, post_url):
 	
 	# 發文ip
 	ip = main_content.select(".f2")[0].text.replace("※ 發信站: 批踢踢實業坊(ptt.cc), 來自: ", "")
-	# print (time, ip)
+	# print (article_time, ip)
 
 	# 發文內容
 	str_main_content = str(main_content)
 	start = str_main_content.find(str(article_time)) + len(article_time) + 13
 	end = str_main_content.find('--')
-	conten = str_main_content[start:end].strip()
+	content = str_main_content[start:end].strip()
+	print (content)
 
 
 	# 推/噓
-	# push = main_content.select(".push")
-	# push_info = list()
-	# for p in push:
-	# 	push_tag = p.select(".push-tag")[0].text
-	# 	push_userid = p.select(".push-userid")[0].text
-	# 	push_content = p.select(".push-content")[0].text
-	# 	push_ipdatetime = p.select(".push-ipdatetime")[0].text.strip()
-	# 	push_info.append([push_tag, push_userid, push_content, push_ipdatetime])
-	# print (push_info)
+	push = main_content.select(".push")
+	push_info = list()
+	for p in push:
+		push_tag = p.select(".push-tag")[0].text
+		push_userid = p.select(".push-userid")[0].text
+		push_content = p.select(".push-content")[0].text[2:]
+		push_ipdatetime = p.select(".push-ipdatetime")[0].text.strip().split()
+		push_ip = push_ipdatetime[0]
+		push_date = push_ipdatetime[1]
+		push_time = push_ipdatetime[2]
+		push_info.append([push_tag, push_userid, push_content, push_ip, push_date, push_time])
+	print (push_info)
 		
 
-	# print (main_content.select("#text"))
 
 
 	
-
-
-
-
-
-
 
 
 # get_board("movie", 1)
