35591c2f5b0c25ddf66167def503f276943ac4e9
shihs <mcshihs@gmail.com>
2019-09-22 20:43:17 +0200

    大改版！

--- PTT.py
+++ PTT.py
@@ -3,12 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
 def get_board(board, pages):
 	'''爬取ptt各版文章資訊
 	Args:
 		borad:看板英文名稱
 		pages:要爬取的頁數，包含最新頁總共的頁數
+	Return:
+		s:requests Session
+		res:get 結果
 	'''
 	s = requests.Session()
 	# 看板
@@ -27,62 +29,80 @@ def get_board(board, pages):
 		s.post(over18_url, data = payload, verify = False)
 		res = s.get(url, verify = False)
 
-
-	# 每頁內容
-	get_page(s, res, board, pages)
+	return (s, res)
 
 
 def get_posts_info(s, page_url):
-	'''獲取該頁文章標題、推文數、作者id
+	'''獲取該頁文章標題、推文數、po文id、文章網址
 	Args:
 		s:get_pages function的Session
 		page_url:每頁的網址
+	Return:
+		posts_info:List，文章標題、推文數、po文id、文章網址
 	'''
 	res = s.get(page_url)
 	soup = BeautifulSoup(res.text, "lxml")
+	
+	# return list
+	posts_info = []
 
-
-	# 獲取文章標題、推文數、作者、文章網址
-	for post in soup.select(".r-ent")[:1]:
-		# python 2
-		# push = post.select(".nrec")[0].text.encode("utf-8").strip()
-		# title = post.select(".title")[0].text.encode("utf-8").strip()
-		# author = post.select(".author")[0].text.encode("utf-8").strip()
+	# 獲取文章標題、推文數、po文id、文章網址
+	for post in soup.select(".r-ent"):
 		
-		# python 3
-		push = post.select(".nrec")[0].text.strip()
-		title = post.select(".title")[0]
-		# 文章的網址
-		post_url = title.select("a")[0]["href"]
-		post_url = "https://www.ptt.cc" + post_url
-		# print (post_url)
-		title = title.text.strip()
-		author = post.select(".author")[0].text.strip()
-		print (push, title, author, post_url)
-		get_post_content(s, post_url)
+		push = post.select(".nrec")[0].text.strip() # 推文數
+
+		try:
+			title = post.select(".title")[0]
+			# 文章網址
+			post_url = title.select("a")[0]["href"] 
+			post_url = "https://www.ptt.cc" + post_url
+			
+			title = title.text.strip() # 文章標題
+			# 略過公告文章
+			if "[公告]" in title:
+				continue
+
+			author = post.select(".author")[0].text.strip() # po文id
+
+			# 將每篇文章簡單資訊儲存到 posts_info_all
+			posts_info.append([push, title, author, post_url, page_url])
+
+		except:
+			continue
+
+	return (posts_info)
 
 
 
 
 def get_page(s, res, board, pages):
-	'''獲取每頁的url並抓每頁文章標題、推文數、作者
+	'''獲取每頁的url並抓每頁文章標題、推文數、po文id、文章網址
 	Args:
 		s:get_pages function的Session
 		res:get_pages function的requests
 		borad:看板英文名稱
 		pages:要爬取的頁數，包含最新頁總共的頁數
+	Return:
+		posts_info_all:List, 多頁的文章標題、推文數、po文id、文章網址
 	'''
 
 	soup = BeautifulSoup(res.text, "lxml")
 	previous = soup.select(".wide")[1]["href"]
 	previous = int(previous[(previous.find("index")+5):previous.find(".html")])
 
+	posts_info_all = []
+
+	# 爬取看板每頁文章簡單資訊
 	for page in range(pages):
+		# 該頁網址
 		page_url = "https://www.ptt.cc/bbs/" + board + "/index" + str(previous - page + 1) + ".html"
 		print (page_url)
-		get_posts_info(s, page_url)
+		# 獲取該頁各文章 推文數、標題、作者、網址
+		posts_info = get_posts_info(s, page_url)
+		# 將每篇文章簡單資訊儲存到 posts_info_all
+		posts_info_all.extend(posts_info)
 
-
+	return (posts_info_all)
 
 
 def get_post_content(s, post_url):
@@ -91,32 +111,43 @@ def get_post_content(s, post_url):
 		s:get_pages function的Session
 		post_url:the url of a post
 	'''
+	print (post_url)
 	res = s.get(post_url)
 	soup = BeautifulSoup(res.text, "lxml")
-
 	main_content = soup.select("#main-content")[0]
 	# print (main_content)
+
 	# 發文時間
 	article_time = main_content.select(".article-metaline")[2].select(".article-meta-value")[0].text
 	# print (article_time)
 	
 	# 發文ip
-	ip = main_content.select(".f2")[0].text.replace("※ 發信站: 批踢踢實業坊(ptt.cc), 來自: ", "")
-	# print (article_time, ip)
+	ips = main_content.select(".f2")
+	ip = ""
+	for i in ips:
+		if "發信站" in str(i):
+			ip = i.text.replace("※ 發信站: 批踢踢實業坊(ptt.cc), 來自: ", "").strip()
+			break
 
 	# 發文內容
 	str_main_content = str(main_content)
 	start = str_main_content.find(str(article_time)) + len(article_time) + 13
 	end = str_main_content.find('--')
+	# end = str_main_content.rfind('--') # 找到最後的 "--"，文章內文結束處
 	content = str_main_content[start:end].strip()
-	print (content)
-
+	content = BeautifulSoup(content, "lxml").text#.replace("\n", "  ")
+	# print (content)
 
 	# 推/噓
-	push = main_content.select(".push")
+	# push = main_content.select(".push")
+	end = str_main_content.rfind('--') # 找到最後的 "--"，文章內文結束處
+	push = str_main_content[end:]
+	push = BeautifulSoup(push, "lxml").select(".push")
+	# print (push)
+
 	push_info = list()
 	for p in push:
-		push_tag = p.select(".push-tag")[0].text
+		push_tag = p.select(".push-tag")[0].text[:-1]
 		push_userid = p.select(".push-userid")[0].text
 		push_content = p.select(".push-content")[0].text[2:]
 		push_ipdatetime = p.select(".push-ipdatetime")[0].text.strip().split()
@@ -124,18 +155,69 @@ def get_post_content(s, post_url):
 		push_date = push_ipdatetime[1]
 		push_time = push_ipdatetime[2]
 		push_info.append([push_tag, push_userid, push_content, push_ip, push_date, push_time])
-	print (push_info)
-		
-
-
-
 	
+	return(article_time, ip, content, push_info)
+
+
+def run(board, pages):
+
+	# 網站session
+	s, res = get_board(board, pages)
+	# print (s, res, board, pages)
+	# post_url = "https://www.ptt.cc/bbs/Gossiping/M.1569170068.A.28B.html"
+	# post_url = "https://www.ptt.cc/bbs/Gossiping/M.1568569396.A.D48.html"
+	# post_content = get_post_content(s, post_url)
+	# print (post_content)
+	
+	posts_info_all = get_page(s, res, board, pages)
+	n = len(posts_info_all)
+
+	all_info = {}
+
+	for i in range(n):
+		post = posts_info_all[i]
+		push = post[0]
+		title = post[1]
+		author = post[2]
+		post_url = post[3]
+		page_url = post[4]
+		article_time, ip, content, push_info = get_post_content(s, post_url)
+		# print (post_content)
+
+		all_info[i+1] = {
+			"title":title,
+			"author":author,
+			"ip":ip,
+			"article_time":article_time,
+			"content":content,
+			"push_info":{
+				"push_number":push,
+				"push":push_info
+			},
+			"post_url":post_url,
+			"page_url":page_url
+
+		}
+
+	return (all_info)
+
+
+
+
+
+
+all_info = run("Gossiping", 1)
+
+for i in all_info:
+	print (all_info[i])
+
+
 
 
 # get_board("movie", 1)
-get_board("Gossiping", 1)
+# posts = get_board("Gossiping", 1)
 
-
+# print (posts)
 
 
 
