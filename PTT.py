a12e8cc940c52545c1471e7396a094ed6627fd35
shihs <mcshihs@gmail.com>
2019-09-23 03:24:45 +0200

    些微調整
    1. 因舊文章有些資訊較不齊全，將程式碼做小修改以處理不同格式的問題
    2. 新增儲存成json

--- PTT.py
+++ PTT.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
 def get_board(board, pages):
@@ -116,23 +117,31 @@ def get_post_content(s, post_url):
 	soup = BeautifulSoup(res.text, "lxml")
 	main_content = soup.select("#main-content")[0]
 	# print (main_content)
+	str_main_content = str(main_content)
 
 	# 發文時間
 	article_time = main_content.select(".article-metaline")[2].select(".article-meta-value")[0].text
 	# print (article_time)
 	
 	# 發文ip
-	ips = main_content.select(".f2")
+	ips = main_content.select(".f2")[:3]
 	ip = ""
 	for i in ips:
 		if "發信站" in str(i):
 			ip = i.text.replace("※ 發信站: 批踢踢實業坊(ptt.cc), 來自: ", "").strip()
+
+			if ip == "※ 發信站: 批踢踢實業坊(ptt.cc)":
+				start = str_main_content.find("◆ From: ") + 8
+				end = str_main_content.find('\n', start)
+				ip = str_main_content[start:end]
 			break
+	# print (ip)
 
 	# 發文內容
-	str_main_content = str(main_content)
+	# str_main_content = str(main_content)
+	# print (str_main_content)
 	start = str_main_content.find(str(article_time)) + len(article_time) + 13
-	end = str_main_content.find('--')
+	end = str_main_content.find('--', start)
 	# end = str_main_content.rfind('--') # 找到最後的 "--"，文章內文結束處
 	content = str_main_content[start:end].strip()
 	content = BeautifulSoup(content, "lxml").text#.replace("\n", "  ")
@@ -147,13 +156,31 @@ def get_post_content(s, post_url):
 
 	push_info = list()
 	for p in push:
-		push_tag = p.select(".push-tag")[0].text[:-1]
+		# print (p.text)
+		try:
+			push_tag = p.select(".push-tag")[0].text[:-1]
+		except:
+			continue
 		push_userid = p.select(".push-userid")[0].text
-		push_content = p.select(".push-content")[0].text[2:]
+		push_content = p.select(".push-content")[0].text[1:].strip()
 		push_ipdatetime = p.select(".push-ipdatetime")[0].text.strip().split()
-		push_ip = push_ipdatetime[0]
-		push_date = push_ipdatetime[1]
-		push_time = push_ipdatetime[2]
+		
+		try:
+			push_ip = push_ipdatetime[0]
+			push_date = push_ipdatetime[1]
+			push_time = push_ipdatetime[2]
+		except:
+			push_ip = ""
+			push_date = ""
+			push_time = ""
+			for j in push_ipdatetime:
+				if "." in j:
+					push_ip = j
+				if "/" in j:
+					push_date = j
+				if ":" in j:
+					push_time = j
+
 		push_info.append([push_tag, push_userid, push_content, push_ip, push_date, push_time])
 	
 	return(article_time, ip, content, push_info)
@@ -163,12 +190,18 @@ def run(board, pages):
 
 	# 網站session
 	s, res = get_board(board, pages)
-	# print (s, res, board, pages)
-	# post_url = "https://www.ptt.cc/bbs/Gossiping/M.1569170068.A.28B.html"
-	# post_url = "https://www.ptt.cc/bbs/Gossiping/M.1568569396.A.D48.html"
-	# post_content = get_post_content(s, post_url)
-	# print (post_content)
+
 	
+	# page_url = "https://www.ptt.cc/bbs/Gossiping/index1.html"
+	# test = get_posts_info(s, page_url)
+	# print (test)
+	# post_url = test[0][3]
+	# post_url = "https://www.ptt.cc/bbs/Gossiping/M.1569198763.A.4BE.html"
+	# print (get_post_content(s, post_url))
+
+
+	
+	# 文章簡單資訊
 	posts_info_all = get_page(s, res, board, pages)
 	n = len(posts_info_all)
 
@@ -181,9 +214,15 @@ def run(board, pages):
 		author = post[2]
 		post_url = post[3]
 		page_url = post[4]
+
+		# page_url.find("index"):
+		# post_id = 
+
+		# 文章詳細資訊
 		article_time, ip, content, push_info = get_post_content(s, post_url)
 		# print (post_content)
 
+		# 整理成 dictionary 格式
 		all_info[i+1] = {
 			"title":title,
 			"author":author,
@@ -199,7 +238,13 @@ def run(board, pages):
 
 		}
 
+	with open("test.json", "w") as f:
+		json.dump(all_info, f)
+
+
+	print ("done!")
 	return (all_info)
+
 
 
 
@@ -208,8 +253,8 @@ def run(board, pages):
 
 all_info = run("Gossiping", 1)
 
-for i in all_info:
-	print (all_info[i])
+# for i in all_info:
+# 	print (all_info[i])
 
 
 
